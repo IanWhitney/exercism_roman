9b5c2e705a25647fc98ab772d56ca15a52aaf17a
Ian Whitney <whit0694@umn.edu>
2015-01-17 13:23:03 -0600

    Add tests for to_neoroman

--- roman_numerals_test.rb
+++ roman_numerals_test.rb
@@ -74,3 +74,77 @@ class RomanTest < MiniTest::Unit::TestCase
     assert_equal 'MMM', 3000.to_roman
   end
 end
+
+class NeoRomanTest < MiniTest::Unit::TestCase
+  def test_1
+    assert_equal 'J', 1.to_neoroman
+  end
+
+  def test_2
+    assert_equal 'JJ', 2.to_neoroman
+  end
+
+  def test_3
+    assert_equal 'JJJ', 3.to_neoroman
+  end
+
+  def test_4
+    assert_equal 'JR', 4.to_neoroman
+  end
+
+  def test_5
+    assert_equal 'R', 5.to_neoroman
+  end
+
+  def test_6
+    assert_equal 'RJ', 6.to_neoroman
+  end
+
+  def test_9
+    assert_equal 'JB', 9.to_neoroman
+  end
+
+  def test_27
+    assert_equal 'BBRJJ', 27.to_neoroman
+  end
+
+  def test_48
+    assert_equal 'BPRJJJ', 48.to_neoroman
+  end
+
+  def test_59
+    assert_equal 'PJB', 59.to_neoroman
+  end
+
+  def test_93
+    assert_equal 'BGJJJ', 93.to_neoroman
+  end
+
+  def test_141
+    assert_equal 'GBPJ', 141.to_neoroman
+  end
+
+  def test_163
+    assert_equal 'GPBJJJ', 163.to_neoroman
+  end
+
+  def test_402
+    assert_equal 'GQJJ', 402.to_neoroman
+  end
+
+  def test_575
+    assert_equal 'QPBBR', 575.to_neoroman
+  end
+
+  def test_911
+    assert_equal 'GOBJ', 911.to_neoroman
+  end
+
+  def test_1024
+    assert_equal 'OBBJR', 1024.to_neoroman
+  end
+
+  def test_3000
+    assert_equal 'OOO', 3000.to_neoroman
+  end
+end
