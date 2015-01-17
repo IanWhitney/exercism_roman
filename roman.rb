4ad1978aa55cc9d1d2377ab0e2bd59be50afd777
Ian Whitney <whit0694@umn.edu>
2015-01-17 13:04:55 -0600

    Introduce conversion parameter
    This is an example of Parameterize Method (Refactoring, p283). I think.
    It's about the closest Refactoring pattern I see to what I'm doing here.
    
    Before, the conversion methods just *knew* what Roman symbols to use.
    This made them impossible to use for the neo-roman feature. By making a
    constant that handles Roman conversion, and then changing the methods to
    work with that conversion constant, we can easily add new conversions..

--- roman.rb
+++ roman.rb
@@ -1,4 +1,6 @@
 class Fixnum
+  ROMAN_CONVERSION = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I"}
+
   def to_roman
     t, rem = to_thousands(self)
     h, rem = to_hundreds(rem)
@@ -10,53 +12,51 @@ class Fixnum
 
   private
 
-  def to_thousands(x)
-    rom = "M" * (x / 1000)
+  def to_thousands(x, conversion = ROMAN_CONVERSION)
+    rom = conversion[1000] * (x / 1000)
     [rom, x.remainder(1000)]
   end
 
-  def to_hundreds(x)
+  def to_hundreds(x, conversion = ROMAN_CONVERSION)
     h = x / 100
     rom = case
     when (h == 9)
-      "CM"
+      "#{conversion[100]}#{conversion[1000]}"
     when (h >= 5)
-      "D" + ("C" * (h - 5))
+      conversion[500] + (conversion[100] * (h - 5))
     when (h == 4)
-      "CD"
+      "#{conversion[100]}#{conversion[500]}"
     else
-      "C" * h
+      conversion[100] * h
     end
     [rom, x.remainder(100)]
   end
 
-  def to_tens(x)
+  def to_tens(x, conversion = ROMAN_CONVERSION)
     t = x / 10
     rom = case
     when (t == 9)
-      "XC"
+      "#{conversion[10]}#{conversion[100]}"
     when (t >= 5)
-      "L" + ("X" * (t - 5))
+      conversion[50] + (conversion[10] * (t - 5))
     when (t == 4)
-      "XL"
-    when (t > 0)
-      "X" * t
+      "#{conversion[10]}#{conversion[50]}"
     else
-      ""
+      conversion[10] * t
     end
     [rom, x.remainder(10)]
   end
 
-  def to_ones(x)
+  def to_ones(x, conversion = ROMAN_CONVERSION)
     case
     when (x == 9)
-      "IX"
+      "#{conversion[1]}#{conversion[10]}"
     when (x >= 5)
-      "V" + ("I" * (x - 5))
+      conversion[5] + (conversion[1] * (x - 5))
     when (x == 4)
-      "IV"
+      "#{conversion[1]}#{conversion[5]}"
     else
-      "I" * x
+      conversion[1] * x
     end
   end
 end
